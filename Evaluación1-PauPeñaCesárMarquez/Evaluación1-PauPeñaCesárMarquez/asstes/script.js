06b85ebf9de85f0f3956caa564237e66863bda2e
cezar17x <checharinmarquez@gmail.com>
2025-04-10 14:32:26 -0400

    1

---
+++ Evaluación1-PauPeñaCesárMarquez/Evaluación1-PauPeñaCesárMarquez/asstes/script.js
@@ -0,0 +1,21 @@
+console.log("Hola mundo! XD");
+name = prompt("Ingrese su nombre:");
+var num1 = Number;
+var num2 = Number;
+alert("Hola " + name);
+function sumatoria()
+{
+    resultado = num1 + num2;
+
+    return resultado;
+}
+
+function agregar()
+{
+    num1 = num1 + 150000
+}
+
+function disminuir()
+{
+    num1 = num1 - 150000
+}
